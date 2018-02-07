b878f246d865818f8a3b60296e0a77041707fb35
Szkolenie <Szkolenie@Blueform>
2018-02-07 11:50:32 +0100

    124f

---
+++ oferta.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <title>Moje Zamówienia</title>
+    </head>
+    <body>
+
+       <?php
+       
+       $conn = mysqli_connect('gitner.pl', 'gitner_login', 'P@ssw0rd123', 'gitner_login', '3306');
+       if (!$conn) {
+           die('Could not connect to MySQL: ' . mysqli_connect_error());
+       }
+       mysqli_query($conn, 'SET NAMES \'utf8\'');
+
+       
+       
+       mysqli_close($conn);
+       
+       ?>
+        
+    </body>
+</html>
