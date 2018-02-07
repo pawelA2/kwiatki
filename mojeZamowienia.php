9bc5982128ee0e48fbcfbc4a16b87574e0cbbd71
Szkolenie <Szkolenie@Blueform>
2018-02-07 10:58:12 +0100

    zmiany w pliku

---
+++ mojeZamowienia.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <title>Moje Zamówienia</title>
+    </head>
+    <body>
+
+        
+        
+    </body>
+</html>
